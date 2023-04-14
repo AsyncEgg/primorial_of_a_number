852da0edf55261b48ed35d7d896b3c1ee89a57f0
Egg55 <emilianomilanog@gmail.com>
2023-04-14 07:17:28 -0600

    speed improvements

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
+use core::num;
 use std::fs::File;
-use std::io::Write;
+use std::io::{Write, BufReader, Read};
+use std::iter::successors;
 use std::thread;
 use std::time::Instant;
 use num_bigint::BigUint;
-use primorial_of_a_number::primes_less_than;
+
+//next step, imrpove to seive ot atkin, and load the prime numbers sequentally instead of storing them in vecs to  see if imoproves
 
 fn main() {
     let thread_count = 7;
@@ -22,11 +25,7 @@ fn main() {
     let mut handles = vec![];
     for chunk in chunks {
         let h = thread::spawn(move || {
-            let mut result = BigUint::from(1_u8);
-            for prime in chunk {
-                result *= BigUint::from(prime);
-            }
-            result
+            return multiply_vec(chunk)
         });
         handles.push(h);
 
@@ -36,16 +35,56 @@ fn main() {
         let result = handle.join().unwrap();
         r*=result;
     }
+    //try to multiply this with fold
     let duration = start.elapsed();
     println!("primorial done {duration:?}");
 
-
     let start = Instant::now();
     
-    let mut file = File::create("n.txt").unwrap();
+    let mut file = File::create("new.txt").unwrap();
     file.write_all(r.to_str_radix(10).as_bytes()).unwrap();
 
     let duration = start.elapsed();
     println!("file done {duration:?}");
+    
+    let mut file = File::open("new.txt").unwrap();
+    let mut contents = String::new();
+    file.read_to_string(&mut contents).unwrap();
+    let num_chars = contents.chars().count();
+    println!("{}",num_chars);
 
+    assert_eq!(num_chars, 6722809)
+}
+
+fn multiply_vec(v: Vec<BigUint>) -> BigUint {
+    let v = v.into_iter().fold(BigUint::from(1_u8), |acc, x| acc * x);
+    BigUint::from(v)
+}
+
+fn primes_less_than(n: u128) -> Vec<BigUint> {
+    
+    if n < 2 {
+        return vec![];
+    }
+    let mut is_prime = vec![true; n as usize];
+    
+    is_prime[0] = false;
+    is_prime[1] = false;
+    
+    for i in 2..f64::sqrt(n as f64) as u128 {
+        if is_prime[i as usize] {
+            for x in (i*i..n).step_by(i as usize) {
+                is_prime[x as usize] = false
+            }
+        }
+    }
+
+    let mut result: Vec<BigUint> = vec![];
+    
+    for i in 0..n {
+        if is_prime[i as usize] {
+            result.push(i.into())
+        }
+    }
+    result
 }
